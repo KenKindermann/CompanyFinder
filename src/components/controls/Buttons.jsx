e19b53bfef6e62dee125b38e239c2f3fb69d239b
kenkindermann <ken.kindermann@gmail.com>
2024-02-28 22:56:10 +0100

    Refactor Button component to fix missing return statement

--- src/components/controls/Buttons.jsx
+++ src/components/controls/Buttons.jsx
@@ -1,6 +1,8 @@
 export const Button = ({ title, icon, onClick }) => {
-  <button onClick={onClick}>
-    {title}
-    <img src={icon} alt={`${title} button`} />
-  </button>;
+  return (
+    <button onClick={onClick}>
+      {title}
+      <img src={icon} alt={`${title} button`} />
+    </button>
+  );
 };
