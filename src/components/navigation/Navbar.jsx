a8923252101ef4a3bac33128b0624c681220118a
kenkindermann <ken.kindermann@gmail.com>
2024-02-28 22:42:31 +0100

    Implement navbar, no function yet

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,12 +1,31 @@
-const Navbar = () => {
-  return (
-    <nav className="bg-custom-blue p-4 flex justify-between items-center text-white gap-8">
-      <span className="text-xl font-bold">CompanyFinder</span>
+import { useState } from "react";
 
-      {/* Search & Bookmark icon */}
-      <div className="flex">
-        <img src="/icons/search.svg" alt="search icon" />
-        <img src="/icons/bookmark.svg" alt="bookmark icon" />
+const Navbar = () => {
+  const [showSearch, setShowSearch] = useState(false);
+
+  return (
+    <nav className="bg-custom-blue ">
+      <div className="flex justify-between items-center text-white gap-8 h-20 p-4 max-w-screen-xl mx-auto">
+        {/* Logo */}
+        <span className={showSearch ? `hidden` : `text-xl font-bold`}>CompanyFinder</span>
+
+        {/* Searchbar */}
+        <input
+          type="text"
+          placeholder="Search company..."
+          className={`p-2 rounded-lg sm:block ${showSearch ? "w-full" : "w-3/4 hidden"}`}
+        />
+
+        {/* Search & Bookmark icon */}
+        <div className={showSearch ? `hidden` : "flex"}>
+          <img
+            src="/icons/search.svg"
+            alt="search icon"
+            onClick={() => setShowSearch(true)}
+            className="block sm:hidden"
+          />
+          <img src="/icons/bookmark.svg" alt="bookmark icon" />
+        </div>
       </div>
     </nav>
   );
