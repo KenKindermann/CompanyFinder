96353a87895bb6694539f9cd3c6306b7af5bab2e
kenkindermann <ken.kindermann@gmail.com>
2024-02-29 00:08:07 +0100

    Implement controlPanel, no function yet

--- src/components/controls/Buttons.jsx
+++ src/components/controls/Buttons.jsx
@@ -2,7 +2,7 @@
 export const Button = ({ title, icon, onClick }) => {
   return (
     <button onClick={onClick} className="custom-button">
-      <img src={icon} alt={`${title} button`} />
+      <img src={icon} alt={`${title} button`} width="25px" />
       {title}
     </button>
   );
@@ -12,7 +12,7 @@ export const Button = ({ title, icon, onClick }) => {
 export const OptionsButton = ({ title, icon, options }) => {
   return (
     <div className="custom-button">
-      <img src={icon} alt={`${title} button`} className={icon ? "block" : "hidden"} />
+      <img src={icon} alt={`${title} button`} className={icon ? "block" : "hidden"} width="25px" />
       <select className="bg-transparent">
         {Object.values(options).map((title, index) => (
           <option key={index} value={title}>
--- src/components/controls/ControlPanel.jsx
+++ src/components/controls/ControlPanel.jsx
@@ -6,11 +6,11 @@ import { apiFieldTitles } from "../../utils/apiFieldTitles.js";
 
 const ControlPanel = () => {
   return (
-    <section className="custom-section flex gap-4 justify-between">
-      <div className="flex gap-2">
+    <section className="custom-section flex flex-col gap-4 sm:flex-row sm:justify-between">
+      <div className="flex gap-2 mb-2">
         <Button title="Filter" icon={filterIcon} />
         <OptionsButton title="Sort" icon={sortIcon} options={apiFieldTitles} />
-        <OptionsButton title="Order" icon={orderIcon} options={{ ascending: "Ascending", descending: "Descending" }} />
+        <OptionsButton title="Order" icon={orderIcon} options={{ ascending: "Asc", descending: "Desc" }} />
       </div>
       <div className="flex items-center gap-2">
         <p>Results per page</p>
