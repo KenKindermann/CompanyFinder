62f6891b23b2774acfcfcc51d27db45918acaf69
kenkindermann <ken.kindermann@gmail.com>
2024-02-28 23:04:53 +0100

    Add ControlPanel component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import ControlPanel from "./components/controls/ControlPanel";
 import Navbar from "./components/navigation/Navbar";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <ControlPanel />
     </>
   );
 };
