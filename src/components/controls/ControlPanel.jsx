2a34286e149106b6472524e129a73767455c89b4
kenkindermann <ken.kindermann@gmail.com>
2024-02-28 23:43:07 +0100

    Add order button to ControlPanel component

--- src/components/controls/ControlPanel.jsx
+++ src/components/controls/ControlPanel.jsx
@@ -1,13 +1,15 @@
 import { Button, OptionsButton } from "../controls/Buttons.jsx";
 import filterIcon from "/public/icons/filter.svg";
 import sortIcon from "/icons/sort.svg";
+import orderIcon from "/icons/order.svg";
 import { apiFieldTitles } from "../../utils/apiFieldTitles.js";
 
 const ControlPanel = () => {
   return (
     <section className="custom-section flex gap-4">
-      <Button title={"Filter"} icon={filterIcon} />
-      <OptionsButton title={"Sort"} icon={sortIcon} options={apiFieldTitles} />
+      <Button title="Filter" icon={filterIcon} />
+      <OptionsButton title="Sort" icon={sortIcon} options={apiFieldTitles} />
+      <OptionsButton title="Order" icon={orderIcon} options={{ ascending: "Ascending", descending: "Descending" }} />
     </section>
   );
 };
