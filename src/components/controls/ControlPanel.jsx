5a5f97fe27f4c2f0178d6cedff73f93a628304f7
kenkindermann <ken.kindermann@gmail.com>
2024-02-28 23:36:11 +0100

    Add filter & options button, no function yet

--- src/components/controls/ControlPanel.jsx
+++ src/components/controls/ControlPanel.jsx
@@ -1,10 +1,13 @@
-import { Button } from "../controls/Buttons.jsx";
+import { Button, OptionsButton } from "../controls/Buttons.jsx";
 import filterIcon from "/public/icons/filter.svg";
+import sortIcon from "/icons/sort.svg";
+import { apiFieldTitles } from "../../utils/apiFieldTitles.js";
 
 const ControlPanel = () => {
   return (
-    <section className="custom-section">
+    <section className="custom-section flex gap-4">
       <Button title={"Filter"} icon={filterIcon} />
+      <OptionsButton title={"Sort"} icon={sortIcon} options={apiFieldTitles} />
     </section>
   );
 };
