92c36bafa485a7adae96c1488fcef30ffe0e74c5
kenkindermann <ken.kindermann@gmail.com>
2024-02-29 20:31:02 +0100

    Implement filter function, filter data is stored in filter state

--- src/components/controls/ControlPanel.jsx
+++ src/components/controls/ControlPanel.jsx
@@ -3,24 +3,50 @@ import filterIcon from "/icons/filter.svg";
 import sortIcon from "/icons/sort.svg";
 import orderIcon from "/icons/order.svg";
 import { apiFieldTitles, sizes, order } from "../../utils/formatter.js";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Popup from "./Popup.jsx";
+import { fetchData } from "../../utils/axiosHelpers.js";
+import { DataContext } from "../provider/DataContext.jsx";
 
 const ControlPanel = () => {
   const [showPopup, setShowPopup] = useState(false);
+  const [filter, setFilter] = useState(null);
+  const { companyData, setCompanyData } = useContext(DataContext);
+
+  useEffect(() => {
+    let params = new URLSearchParams(filter).toString();
+    params = params.replace(/\+/g, "%20");
+    const query = `?${params}`;
+    getSearchResults(query);
+  }, [filter]);
+
+  const getSearchResults = async (query) => {
+    const searchResults = await fetchData(query);
+    setCompanyData(searchResults);
+  };
 
   return (
-    <section className="custom-section flex flex-col gap-4 sm:flex-row sm:justify-between">
+    <section className="custom-section flex items-center flex-col gap-4 sm:flex-row sm:justify-between ">
       <div className="flex gap-2 mb-2">
         <Button title="Filter" icon={filterIcon} onClick={() => setShowPopup(true)} />
         <OptionsButton title="Sort" icon={sortIcon} options={apiFieldTitles} controlKey={"sort"} />
         <OptionsButton title="Order" icon={orderIcon} options={order} controlKey={"desc"} />
       </div>
+
       <div className="flex items-center gap-2">
         <p>Results per page</p>
         <OptionsButton options={sizes} controlKey={"size"} />
       </div>
-      {showPopup && <Popup title={"Filter"} inputs={apiFieldTitles} setShowPopup={setShowPopup} />}
+
+      {showPopup && (
+        <Popup
+          title={"Filter"}
+          inputs={apiFieldTitles}
+          setShowPopup={setShowPopup}
+          filter={filter}
+          setFilter={setFilter}
+        />
+      )}
     </section>
   );
 };
