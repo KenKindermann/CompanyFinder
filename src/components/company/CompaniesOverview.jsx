902996d696e573e799d062a48bb2495fa29aeeba
kenkindermann <ken.kindermann@gmail.com>
2024-02-29 08:34:43 +0100

    Store fetched companies in state on page load, render CompanyCard for each entry

--- src/components/company/CompaniesOverview.jsx
+++ src/components/company/CompaniesOverview.jsx
@@ -3,21 +3,20 @@ import { fetchData } from "../../utils/axiosHelpers.js";
 import CompanyCard from "./CompanyCard.jsx";
 
 const CompaniesOverview = () => {
-  const [companies, setCompanies] = useState([]);
+  const [companies, setCompanies] = useState(null);
 
   /* Get companies by loading page */
   useEffect(() => {
     const getCompanies = async () => {
       const companies = await fetchData();
-      console.log(companies);
+      setCompanies(companies);
     };
     getCompanies();
   }, []);
 
   return (
-    <section className="custom-section">
-      <CompanyCard />
-      <div>sdfsdfsd</div>
+    <section className="custom-section grid sm:grid-cols-2 gap-4">
+      {companies && companies.map((company) => <CompanyCard key={company.id} company={company} />)}
     </section>
   );
 };
