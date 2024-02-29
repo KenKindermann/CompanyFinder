402d904d3952305557d54db3fb12eec0f9948314
kenkindermann <ken.kindermann@gmail.com>
2024-02-29 12:10:28 +0100

    Update ControlPanel component with new options and control keys

--- src/components/controls/ControlPanel.jsx
+++ src/components/controls/ControlPanel.jsx
@@ -2,19 +2,19 @@ import { Button, OptionsButton } from "../controls/Buttons.jsx";
 import filterIcon from "/icons/filter.svg";
 import sortIcon from "/icons/sort.svg";
 import orderIcon from "/icons/order.svg";
-import { apiFieldTitles } from "../../utils/apiFieldTitles.js";
+import { apiFieldTitles, sizes, order } from "../../utils/Formatter.js";
 
 const ControlPanel = () => {
   return (
     <section className="custom-section flex flex-col gap-4 sm:flex-row sm:justify-between">
       <div className="flex gap-2 mb-2">
         <Button title="Filter" icon={filterIcon} />
-        <OptionsButton title="Sort" icon={sortIcon} options={apiFieldTitles} />
-        <OptionsButton title="Order" icon={orderIcon} options={{ ascending: "Asc", descending: "Desc" }} />
+        <OptionsButton title="Sort" icon={sortIcon} options={apiFieldTitles} controlKey={"sort"} />
+        <OptionsButton title="Order" icon={orderIcon} options={order} controlKey={"desc"} />
       </div>
       <div className="flex items-center gap-2">
         <p>Results per page</p>
-        <OptionsButton options={{ 10: 10, 25: 25, 50: 50 }} />
+        <OptionsButton options={sizes} controlKey={"size"} />
       </div>
     </section>
   );
