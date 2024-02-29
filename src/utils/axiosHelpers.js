b6523dc53f6a398a0d7c4b3f2f4680a4b29548a9
kenkindermann <ken.kindermann@gmail.com>
2024-02-29 21:23:37 +0100

    Refactor fetchData function to get totalElements

--- src/utils/axiosHelpers.js
+++ src/utils/axiosHelpers.js
@@ -4,7 +4,7 @@ export const fetchData = async (query = "") => {
   const url = "https://exercises.batix.help/api/v2/company/";
   try {
     const response = await axios.get(url + query);
-    return response.data.content ?? response.data;
+    return response.data;
   } catch (error) {
     console.log(error);
   }
