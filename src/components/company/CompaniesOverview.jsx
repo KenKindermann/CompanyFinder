23635325dfe71ac57d0ff9194e6f8a013de62254
kenkindermann <ken.kindermann@gmail.com>
2024-02-29 16:59:04 +0100

    Remove unused variable

--- src/components/company/CompaniesOverview.jsx
+++ src/components/company/CompaniesOverview.jsx
@@ -3,7 +3,7 @@ import CompanyCard from "./CompanyCard.jsx";
 import { DataContext } from "../provider/DataContext";
 
 const CompaniesOverview = () => {
-  const { companyData, setCompanyData } = useContext(DataContext);
+  const { companyData } = useContext(DataContext);
 
   return (
     <section className="custom-section grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
