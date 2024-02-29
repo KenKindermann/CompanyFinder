24b1c9877693ddfc986f2738a52e1c095074393d
kenkindermann <ken.kindermann@gmail.com>
2024-02-29 11:44:36 +0100

    Add controlContext

--- src/components/provider/ControlContext.jsx
+++ src/components/provider/ControlContext.jsx
@@ -1,9 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { fetchData } from "../../utils/axiosHelpers";
+import { DataContext } from "./DataContext";
 
 export const ControlContext = createContext();
 
 export const ControlProvider = ({ children }) => {
-  const [control, setControl] = useState(null);
+  const { setCompanyData } = useContext(DataContext);
+  const [control, setControl] = useState({
+    desc: false,
+    page: 1,
+    size: 10,
+    sort: "company",
+  });
+
+  useEffect(() => {
+    const query = `?page=${control.page}&size=${control.size}&sort=${control.sort}&desc=${control.desc}`;
+    console.log(query);
+    getCompanies(query);
+  }, [control]);
+
+  const getCompanies = async (query) => {
+    const companies = await fetchData(query);
+    setCompanyData(companies);
+  };
 
   const value = {
     control,
