be1fed84902babd0ba3be05aff0292476cf9743b
kenkindermann <ken.kindermann@gmail.com>
2024-02-29 16:25:31 +0100

    Add favorites state and localStorage useEffect

--- src/components/provider/DataContext.jsx
+++ src/components/provider/DataContext.jsx
@@ -1,13 +1,20 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const DataContext = createContext();
 
 export const DataProvider = ({ children }) => {
   const [companyData, setCompanyData] = useState(null);
+  const [favorites, setFavorites] = useState(JSON.parse(window.localStorage.getItem("CompanyFinderFavorites")) || []);
+
+  useEffect(() => {
+    window.localStorage.setItem("CompanyFinderFavorites", JSON.stringify(favorites));
+  }, [favorites]);
 
   const value = {
     companyData,
     setCompanyData,
+    favorites,
+    setFavorites,
   };
 
   return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
