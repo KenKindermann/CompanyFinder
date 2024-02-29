f73b5e5f25173dbb2ace2108a14c01a01db93d15
kenkindermann <ken.kindermann@gmail.com>
2024-02-29 20:30:18 +0100

    Implement filter function in popup

--- src/components/controls/Popup.jsx
+++ src/components/controls/Popup.jsx
@@ -2,19 +2,53 @@ import { Button } from "./Buttons";
 import filterIcon from "/icons/filter.svg";
 import closeIcon from "/icons/close.svg";
 
-const Popup = ({ title, inputs, setShowPopup }) => {
+const Popup = ({ title, inputs, setShowPopup, filter, setFilter }) => {
+  // FormData to object and hide popup by submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    formDataToObject(e.target);
+    setShowPopup(false);
+  };
+
+  // Format formData to object and set object in filter state
+  const formDataToObject = (e) => {
+    const formData = new FormData(e);
+    const dataObject = {};
+
+    for (let [name, value] of formData) {
+      if (value.trim() !== "") {
+        dataObject[`search_${name}`] = value;
+      }
+    }
+    setFilter(dataObject);
+  };
+
   return (
     <>
       <div className="absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full sm:w-1/2 min-h-96 bg-white z-20 rounded-lg shadow-lg">
         <div className="bg-custom-blue p-5 text-white rounded-tl-lg rounded-tr-lg">{title}</div>
-        <div className="grid grid-cols-2 gap-4 p-4">
-          {inputs.map((input) => (
-            <input key={input.label} className="p-2 rounded-lg border outline-custom-blue" placeholder={input.label} />
-          ))}
-        </div>
-        <div className="flex justify-center mb-4">
-          <Button title={"Add Filter"} icon={filterIcon} />
-        </div>
+
+        {/* Form */}
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div className="grid grid-cols-2 gap-4 p-4">
+            {inputs.map((input) => (
+              <input
+                key={input.label}
+                name={input.apiName}
+                className="p-2 rounded-lg border outline-custom-blue"
+                placeholder={input.label}
+                defaultValue={(filter && filter[`search_${input.apiName}`]) || ""}
+              />
+            ))}
+          </div>
+
+          {/* Submit button */}
+          <div className="flex justify-center mb-4">
+            <Button title={"Filter"} icon={filterIcon} />
+          </div>
+        </form>
+
+        {/* Close button*/}
         <img
           src={closeIcon}
           width="25px"
@@ -23,6 +57,7 @@ const Popup = ({ title, inputs, setShowPopup }) => {
           onClick={() => setShowPopup(false)}
         />
       </div>
+
       {/* Dark overlay*/}
       <div
         onClick={() => setShowPopup(false)}
